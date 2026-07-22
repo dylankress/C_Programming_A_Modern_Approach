fdf427563018331f2dc3a573ce3101c96806cf8a
dylankress <dylankress@gmail.com>
2026-07-22 10:42:34 -0500

    added solutions for chapter 3 projects 1 & 2

--- programming_projects/chapter_03/project_01.c
+++ programming_projects/chapter_03/project_01.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+
 int main(void)
 {
+	int month = 0;
+	int day = 0;
+	int year = 0;
+
+	printf("Please enter a date in mm/dd/yyyy format... ");
+	scanf("%d/%d/%d", &month, &day, &year);
+
+	printf("You entered the date %.4d%.2d%.2d.\n", year, month, day);	
+	
 	return 0;
 }
--- programming_projects/chapter_03/project_02.c
+++ programming_projects/chapter_03/project_02.c
@@ -1,4 +1,24 @@
+#include <stdio.h>
+
 int main(void)
 {
+	int item_number = 0;
+	float unit_price = 0.0f;
+	int purchase_date_day = 0;
+	int purchase_date_month = 0;
+	int purchase_date_year = 0;
+
+	printf("Enter item number: ");
+	scanf("%d", &item_number);
+
+	printf("Enter unit price: ");
+	scanf("%f", &unit_price);
+
+	printf("Enter purchase date (mm/dd/yyyy): ");
+	scanf("%d/%d/%d", &purchase_date_month, &purchase_date_day, &purchase_date_year);
+
+	printf("Item\t\tUnit\t\tPurchase\n\t\tPrice\t\tDate\n");
+	printf("%d\t\t$%7.2f\t%-.2d/%-.2d/%-.4d\n", item_number, unit_price, purchase_date_month, purchase_date_day, purchase_date_year);
+
 	return 0;
 }
