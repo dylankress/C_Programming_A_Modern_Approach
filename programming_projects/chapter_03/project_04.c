4b222db252f4e8584e60831570b6af0dfaaa146d
dylankress <dylankress@gmail.com>
2026-07-22 11:09:23 -0500

    added solution for chapter 3 project 4

--- programming_projects/chapter_03/project_04.c
+++ programming_projects/chapter_03/project_04.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+
 int main(void)
 {
+	int phone_number_area_code = 0;
+	int phone_number_first_three = 0;
+	int phone_number_last_four = 0;
+
+	printf("Enter phone number [(xxx) xxx-xxxx]: ");
+	scanf("(%d) %d-%d", &phone_number_area_code, &phone_number_first_three, &phone_number_last_four);
+
+	printf("You entered %d.%d.%d\n", phone_number_area_code, phone_number_first_three, phone_number_last_four);
+
 	return 0;
 }
