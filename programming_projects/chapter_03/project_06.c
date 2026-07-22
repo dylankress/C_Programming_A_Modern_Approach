d4cb824182e343f868f389f24e7da9f3d41da399
dylankress <dylankress@gmail.com>
2026-07-22 11:58:51 -0500

    added solution for chapter 3 project 6

--- programming_projects/chapter_03/project_06.c
+++ programming_projects/chapter_03/project_06.c
@@ -1,4 +1,21 @@
+#include <stdio.h>
+
 int main(void)
 {
+	int num1 = 0;
+	int denom1 = 0;
+	int num2 = 0;
+	int denom2 = 0;
+	int result_num = 0;
+	int result_denom = 0;
+
+	printf("Enter 2 fractions separated by a plus sign to add them together (x/x + x/x): ");
+	scanf("%d/%d + %d/%d", &num1, &denom1, &num2, &denom2);
+
+	result_num = num1 * denom2 + num2 * denom1;
+	result_denom = denom1 * denom2;
+
+	printf("The sum is: %d/%d\n", result_num, result_denom);
+
 	return 0;
 }
