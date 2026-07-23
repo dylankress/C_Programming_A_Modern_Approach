5519abd5410261259cf4f7582495b8bff9337a88
dylankress <dylankress@gmail.com>
2026-07-23 12:15:31 -0500

    sdded solutions for chapter 4 projects 1 & 2

---
+++ programming_projects/chapter_04/project_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int number = 0;
+
+	printf("Enter a 2 digit number: ");
+	scanf("%d", &number);
+
+	printf("The reversal is: %d%d\n", number % 10, number / 10);
+
+	return 0;
+}
